252850d2ddc5169d49c173eff528701c3a7bf48c
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-04-26 22:32:06 +0100

    Better implementation of the to_json conversion for document

--- lib/rackjson/document.rb
+++ lib/rackjson/document.rb
@@ -17,17 +17,16 @@ module Rack::JSON
       @document.send(name, *args)
     end
 
-    def to_json
+    def to_json(*a)
       unless @json
         gen_attrs = @document.attributes
         gen_attrs.each_pair do |key, value|
-
           if value.is_a? Mongo::ObjectID
             gen_attrs[key] = gen_attrs[key].to_s
           end
         end
       end
-      @json ||= JSON.generate gen_attrs
+      gen_attrs.to_json
     end
   end
 end
