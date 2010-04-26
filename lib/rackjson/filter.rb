46adc957a47f721712a705756f1e43b8e1f14154
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-04-26 22:58:58 +0100

    rename some methods in access filter

--- lib/rackjson/filter.rb
+++ lib/rackjson/filter.rb
@@ -18,7 +18,7 @@ module Rack::JSON
 
     private
 
-    def append_filter_params_to_document_in request
+    def append_filters_to_document_in request
       document = Rack::JSON::Document.new(request.json)
       document.add_attributes(request.session.reject { |key, value| !@filters.include?(key.to_sym) })
       request.set_body(document.to_json)
@@ -26,10 +26,10 @@ module Rack::JSON
 
     def apply_filters(request)
       @filters.each do |filter|
-        return pre_condition_not_met_for filter unless request.session[filter.to_s]
+        return pre_condition_not_met unless request.session[filter.to_s]
         request.add_query_param "[?#{filter}=#{request.session[filter.to_s]}]"
       end
-      append_filter_params_to_document_in request if request.post? || request.put?
+      append_filters_to_document_in request if request.post? || request.put?
       @app.call(request.env)
     end
 
@@ -37,7 +37,7 @@ module Rack::JSON
       request.collection.empty? || !(@collections.include? request.collection.to_sym)
     end
 
-    def pre_condition_not_met_for filter
+    def pre_condition_not_met
       Rack::JSON::Response.new("pre condition not met", :status => 412, :head => true).to_a
     end
   end
