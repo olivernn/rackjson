0d222563b4676ba547b33197d1f179f775a09edf
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-04-27 11:59:03 +0100

    remove symbolize_keys, not used

--- lib/rackjson.rb
+++ lib/rackjson.rb
@@ -1,25 +1,3 @@
-class Hash
-  def symbolize_keys
-    inject({}) do |options, (key, value)|
-      options[(key.to_sym rescue key) || key] = value
-      options
-    end
-  end
-
-  def symbolize_keys!
-    self.replace(self.symbolize_keys)
-  end
-
-  def recursive_symbolize_keys!
-    symbolize_keys!
-    # symbolize each hash in .values
-    values.each{|h| h.recursive_symbolize_keys! if h.is_a?(Hash) }
-    # symbolize each hash inside an array in .values
-    values.select{|v| v.is_a?(Array) }.flatten.each{|h| h.recursive_symbolize_keys! if h.is_a?(Hash) }
-    self
-  end
-end
-
 require 'rubygems'
 require 'json'
 require 'rack'
